b594bc9011245c372f5af99d4cdbce4a1288806b
Rózsa István <ristvan98@gmail.com>
2023-03-19 13:27:58 +0100

    Update

--- classes/Jegy.php
+++ classes/Jegy.php
@@ -17,16 +17,16 @@ class Jegy
     public function insert($booking)
     {
         $msg = "";
-        $jegy_insert = $this->db->insert("jegy", array("fizetve" => ":fizetve", "fizetes_mod" => ":fizetes_mod", "repulo_id" => ":repulo_id", "felhasznalo_id" => ":felhasznalo_id"), array(":fizetve" => "0", ":fizetes_mod" => $booking["fizetes_mod"], ":repulo_id" => $booking["repulo_id"], ":felhasznalo_id" => $booking["felhasznalo_id"]));
+        $jegy_insert = $this->db->insert("foglalas", array("fizetve" => ":fizetve", "fizetes_mod" => ":fizetes_mod", "repulo_id" => ":repulo_id", "felhasznalo_id" => ":felhasznalo_id"), array(":fizetve" => "0", ":fizetes_mod" => $booking["fizetes_mod"], ":repulo_id" => $booking["repulo_id"], ":felhasznalo_id" => $booking["felhasznalo_id"]));
         if (!$jegy_insert) {
             $msg = "Sikertelen jegy hozzáadás";
         }
-        $jegy_id = $this->db->select("SELECT MAX(id) as max_jegy_id FROM jegy");
+        $jegy_id = $this->db->select("SELECT MAX(id) as max_jegy_id FROM foglalas");
         $jegy_id = $this->db->fetchObject($jegy_id);
         $ar = $booking["ar"];
         foreach ($booking as $key => $value) {
             if (is_array($value)) {
-                $this->db->insert("jegy_adatok", array("utas_kernev" => ":utas_kernev", "utas_veznev" => ":utas_veznev", "utas_szulido" => ":utas_szulido", "ules" => ":ules", "ar" => ":ar", "jegy_id" => ":jegy_id"), array(":utas_kernev" => $value["utas_kernev"], ":utas_veznev" => $value["utas_veznev"], ":utas_szulido" => date("y-M-d", strtotime($value["utas_szulido"])), ":ules" => $value["ules"], ":ar" => $ar, ":jegy_id" => $jegy_id->MAX_JEGY_ID));
+                $this->db->insert("jegy", array("utas_kernev" => ":utas_kernev", "utas_veznev" => ":utas_veznev", "utas_szulido" => ":utas_szulido", "ules" => ":ules", "ar" => ":ar", "foglalas_id" => ":foglalas_id"), array(":utas_kernev" => $value["utas_kernev"], ":utas_veznev" => $value["utas_veznev"], ":utas_szulido" => date("y-M-d", strtotime($value["utas_szulido"])), ":ules" => $value["ules"], ":ar" => $ar, ":foglalas_id" => $jegy_id->MAX_JEGY_ID));
             }
         }
         $response = [
@@ -37,7 +37,7 @@ class Jegy
 
     public function getLefoglaltUlohelyek($repulo_id)
     {
-        $query = $this->db->select("SELECT jegy_adatok.ules FROM jegy_adatok, jegy WHERE jegy_adatok.jegy_id = jegy.id AND jegy.repulo_id = :repulo_id", array(":repulo_id" => $repulo_id));
+        $query = $this->db->select("SELECT jegy.ules FROM jegy, foglalas WHERE jegy.foglalas_id = foglalas.id AND foglalas.repulo_id = :repulo_id", array(":repulo_id" => $repulo_id));
         $ulesek = array();
         while ($row = $this->db->fetchArray($query)) {
             array_push($ulesek, $row["ULES"]);
@@ -47,7 +47,7 @@ class Jegy
 
     public function getFoglalasok()
     {
-        $query = $this->db->select("SELECT jegy.*, repulo.indulasi_ido, repulo.erkezesi_ido, repulo.indulasi_datum, repulo.erkezesi_datum, (SELECT repuloter.nev FROM repuloter WHERE repuloter.id = repulo.indulo_repuloter_id) as indulasi_repter, (SELECT repuloter.nev FROM repuloter WHERE repuloter.id = repulo.erkezo_repuloter_id) as erkezo_repter FROM jegy, repulo WHERE jegy.repulo_id = repulo.id");
+        $query = $this->db->select("SELECT foglalas.*, repulo.indulasi_ido, repulo.erkezesi_ido, repulo.indulasi_datum, repulo.erkezesi_datum, (SELECT repuloter.nev FROM repuloter WHERE repuloter.id = repulo.indulo_repuloter_id) as indulasi_repter, (SELECT repuloter.nev FROM repuloter WHERE repuloter.id = repulo.erkezo_repuloter_id) as erkezo_repter FROM foglalas, repulo WHERE foglalas.repulo_id = repulo.id");
         $foglalasok = array();
         while ($row = $this->db->fetchArray($query)) {
             $row["ERKEZESI_DATUM"] = date("Y-m-d", strtotime($row["ERKEZESI_DATUM"]));
@@ -59,7 +59,7 @@ class Jegy
 
     public function getFoglalasAdatok($jegy_id)
     {
-        $query = $this->db->select("SELECT * FROM jegy_adatok WHERE jegy_id = :jegy_id", array(":jegy_id" => $jegy_id));
+        $query = $this->db->select("SELECT * FROM jegy WHERE foglalas_id = :foglalas_id", array(":foglalas_id" => $jegy_id));
         $foglalasok = array();
         while ($row = $this->db->fetchArray($query)) {
             $row["UTAS_SZULIDO"] = date("Y-m-d", strtotime($row["UTAS_SZULIDO"]));
@@ -70,22 +70,22 @@ class Jegy
 
     public function fizetve($jegy_id)
     {
-        $this->db->update("jegy", array("fizetve" => ":fizetve"), "id = '" . $jegy_id . "'", array(":fizetve" => "1"));
+        $this->db->update("foglalas", array("fizetve" => ":fizetve"), "id = '" . $jegy_id . "'", array(":fizetve" => "1"));
     }
 
     public function deleteJegy($jegy_id)
     {
-        $this->db->delete("jegy", "id = :id", array(":id" => $jegy_id));
+        $this->db->delete("foglalas", "id = :id", array(":id" => $jegy_id));
     }
 
     public function deleteJegyAdat($id)
     {
-        $this->db->delete("jegy_adatok", "id = :id", array(":id" => $id));
+        $this->db->delete("jegy", "id = :id", array(":id" => $id));
     }
 
     public function getJegyAdatok($id)
     {
-        $query = $this->db->select("SELECT * FROM jegy_adatok WHERE id = :id", array(":id" => $id));
+        $query = $this->db->select("SELECT * FROM jegy WHERE id = :id", array(":id" => $id));
         return $this->db->fetchObject($query);
     }
 
